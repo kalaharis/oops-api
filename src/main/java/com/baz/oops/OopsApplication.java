a22ca7454de7d299b6f7439c622ec88423b884fa
kalaharis <19behappy91@gmail.com>
2017-06-09 11:03:24 +0300

    Initial commit

---
+++ src/main/java/com/baz/oops/OopsApplication.java
@@ -0,0 +1,12 @@
+package com.baz.oops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/baz/oops/OopsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.baz.oops;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OopsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
