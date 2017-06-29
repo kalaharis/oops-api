f6bb2ee5fd71fe69b432bad750d34c5f93a10cff
kalaharis <19behappy91@gmail.com>
2017-06-29 15:04:38 +0300

    cors mapping fixed

--- src/main/java/com/baz/oops/OopsApplication.java
+++ src/main/java/com/baz/oops/OopsApplication.java
@@ -1,6 +1,8 @@
 package com.baz.oops;
 
+import com.baz.oops.api.JSON.CreatePollRequest;
 import com.baz.oops.persistence.PollsRepository;
+import com.baz.oops.service.PollService;
 import com.baz.oops.service.enums.State;
 import com.baz.oops.service.model.Option;
 import com.baz.oops.service.model.Poll;
@@ -26,20 +28,21 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @SpringBootApplication
 public class OopsApplication implements CommandLineRunner {
+    //TODO: remove at production
     @Autowired
-    private PollsRepository pollsRepository;
+    private PollService pollService;
 
     public static void main(String[] args) {
         SpringApplication.run(OopsApplication.class, args);
     }
 
-    //TODO: uncomment after dev
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("")
+                log.info("Configuring cors...");
+                registry.addMapping("/**")
                         .allowedMethods("POST", "GET", "DELETE", "PUT")
                         .allowedOrigins("http://localhost:4200");
             }
@@ -47,15 +50,15 @@ public class OopsApplication implements CommandLineRunner {
     }
 
 
-    //TODO: remove a production
+    //TODO: remove at production
     @Override
     public void run(String... strings) throws Exception {
-        /*Random rnd = new Random();
+        Random rnd = new Random();
         String[] testTags = {"series", "games", "TV", "internet", "cats"};
         for (int i = 0; i < 10; i++) {
 
             List<Option> options = new ArrayList<>();
-            for (int j = 0; j < rnd.nextInt(5); j++) {
+            for (int j = 0; j < rnd.nextInt(5) + 2; j++) {
                 Option option = new Option("option " + j);
                 options.add(option);
             }
@@ -65,11 +68,10 @@ public class OopsApplication implements CommandLineRunner {
                 tags.add(testTags[rnd.nextInt(testTags.length)]);
             }
 
-            Poll poll = new Poll("poll " + i, options);
-            poll.setTotalVotes(i);
-            poll.setTags(tags);
+            CreatePollRequest req = new CreatePollRequest("poll " + i, options);
+            req.setTags(tags);
 
-            pollsRepository.save(poll);
-        }*/
+            pollService.createPoll(req);
+        }
     }
 }
